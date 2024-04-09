93c1e75edcc3177649a3a56b895b82579de6cecf
arv-n <arvind-nayak@outlook.com>
2024-04-09 20:50:02 +0200

    Add template for cover letter

---
+++ coverletter/auto/main.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "src/Preamble"
+    "Txt/Application-letter"
+    "article"
+    "art10"
+    "inputenc"
+    "src/Application"))
+ :latex)
+
